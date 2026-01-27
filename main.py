1fb218958358c920ef7b926a6b3979a1b558117e
Aheadgo <122512546+Aheadgo@users.noreply.github.com>
2026-01-27 16:28:27 +0800

    create

---
+++ main.py
@@ -0,0 +1,26 @@
+import asyncio
+from demo_md import Binance_md, bar_notifier
+from utility import Bardata
+
+# 使用on_bar作为回调函数
+async def on_bar(bar: Bardata):
+  print(f"{bar.symbol} {bar.timestamp} {bar.close}")
+
+async def main():
+  Marketdata_Provider = Binance_md()
+  Bar_Notifier = bar_notifier()
+
+  await Marketdata_Provider.initialize({
+    "url": "wss://stream.testnet.binance.vision:9443/ws"
+  })
+
+  Bar_Notifier.register_bar_callback("BTCUSDT", "1s", on_bar)
+
+  await Marketdata_Provider.subscribe_bars(["BTCUSDT"], "1s", Bar_Notifier._notify_bar_completion)
+
+  while True:
+    await asyncio.sleep(1)
+
+if __name__ == "__main__":
+  asyncio.run(main())
+
